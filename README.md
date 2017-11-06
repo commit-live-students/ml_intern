b57da4367cf59dc8c316e3f8b519b9165f5ee94e
jay-trivedi <mail.jaytrivedi@gmail.com>
2017-11-06 12:36:56 +0530

    updates

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ml_intern
+# Machine Learning Screening Assignments:
+
+This repo contains two Assignments,
+
+1. olympics project
+2. complex numbers
+
+Problem statements are in the respective folders.
---
+++ olympics-mini-project/build.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+def load_data():
+    """
+        Enter your code here
+    """
+
+
+def first_country(df):
+    """
+        Enter your code here
+    """
+
+
+def gold_medal(df):
+    """
+        Enter your code here
+    """
+
+
+def biggest_difference_in_gold_medal(df):
+    """
+        Enter your code here
+    """
+
+def get_points(df):
+    """
+    Enter your code here
+    """
